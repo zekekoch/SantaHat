53bbff14341bce877f0723252c2032829ea55384
Zeke Koch <zeke@akrasia.org>
2013-11-09 04:07:16 -0800

    simplified rainbow march and color bounce

--- SantaHat.ino
+++ SantaHat.ino
@@ -1,33 +1,27 @@
 #include "FastSPI_LED2.h"
 
+#define ledCount 15
+#define PIN 3
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-// test code
-//
-//////////////////////////////////////////////////
-
-#define NUM_LEDS 15
-
-CRGB leds[NUM_LEDS];
-int ledsX[NUM_LEDS][3];     //-ARRAY FOR COPYING WHATS IN THE LED STRIP CURRENTLY (FOR CELL-AUTOMATA, MARCH, ETC)
+CRGB leds[ledCount];
 int BOTTOM_INDEX = 0;
-int TOP_INDEX = int(NUM_LEDS/2);
-int EVENODD = NUM_LEDS%2;
+int TOP_INDEX = int(ledCount/2);
+int EVENODD = ledCount%2;
 
 
 void setup() {
 	// sanity check delay - allows reprogramming if accidently blowing power w/leds
-   	delay(2000);
+   	delay(1000);
 
-   	LEDS.setBrightness(255);
-   	LEDS.addLeds<WS2811, 3, GRB>(leds, NUM_LEDS);
+    // global brightness doesn't work on the attiny85
+   	//LEDS.setBrightness(8);
+   	LEDS.addLeds<WS2811, PIN, GRB>(leds, ledCount);
 }
 
 void rainbow_loop(){                       //-m88-RAINBOW FADE FROM FAST_SPI2
   static byte ihue = 0;
   ihue -= 1;
-  fill_rainbow( leds, NUM_LEDS, ihue );
+  fill_rainbow( leds, ledCount, ihue );
   LEDS.show();
   delay(5);
 }
@@ -38,20 +32,20 @@ int horizontal_index(int i) {
   if (i == BOTTOM_INDEX) {return BOTTOM_INDEX;}
   if (i == TOP_INDEX && EVENODD == 1) {return TOP_INDEX + 1;}
   if (i == TOP_INDEX && EVENODD == 0) {return TOP_INDEX;}
-  return NUM_LEDS - i;  
+  return ledCount - i;  
 }
 
 //---FIND INDEX OF ANTIPODAL OPPOSITE LED
 int antipodal_index(int i) {
   int iN = i + TOP_INDEX;
-  if (i >= TOP_INDEX) {iN = ( i + TOP_INDEX ) % NUM_LEDS; }
+  if (i >= TOP_INDEX) {iN = ( i + TOP_INDEX ) % ledCount; }
   return iN;
 }
 
 //---FIND ADJACENT INDEX CLOCKWISE
 int adjacent_cw(int i) {
   int r;
-  if (i < NUM_LEDS - 1) {r = i + 1;}
+  if (i < ledCount - 1) {r = i + 1;}
   else {r = 0;}
   return r;
 }
@@ -60,46 +54,69 @@ int adjacent_cw(int i) {
 int adjacent_ccw(int i) {
   int r;
   if (i > 0) {r = i - 1;}
-  else {r = NUM_LEDS - 1;}
+  else {r = ledCount - 1;}
   return r;
 }
 
-
-void copy_led_array(){
-  for(int i = 0; i < NUM_LEDS; i++ ) {
-    ledsX[i][0] = leds[i].r;
-    ledsX[i][1] = leds[i].g;
-    ledsX[i][2] = leds[i].b;
-  }  
+void marchCW()
+{
+  for(int i = ledCount; i > 0; i-- ) 
+  {
+    leds[i] = leds[i-1];
+  }
+  leds[0] = leds[ledCount-1];
 }
 
+void marchCCW()
+{
 
-void rwb_march() {                    //-m15-R,W,B MARCH CCW
-  copy_led_array();
-  int iCCW;
-  static int idex = 0;
-  idex++;
-  if (idex > 2) {idex = 0;}
-  switch (idex) {
-    case 0:
-      leds[0] = CRGB::Red;
-    break;
-    case 1:
-      leds[0] = CRGB::White;
-    break;
-    case 2:
-      leds[0] = CRGB::Green;
-    break;
-  }
-    for(int i = 1; i < NUM_LEDS; i++ ) {
-      iCCW = adjacent_ccw(i);
-      leds[i].r = ledsX[iCCW][0];
-      leds[i].g = ledsX[iCCW][1];
-      leds[i].b = ledsX[iCCW][2];    
+}
+
+void threeColorFill(CRGB first, CRGB second, CRGB third)
+{
+  for (int i = 0;i<ledCount;i++)
+  {
+    switch(i%3)
+    {
+      case 0:
+        leds[i] = first;
+        break;
+      case 1:
+        leds[i] = second;
+        break;
+      case 2:
+        leds[i] = third;
+        break;
     }
-  LEDS.show();  
+  }
+}
+
+void rwb_march(boolean firstRun)
+{
+  if (firstRun) 
+  {
+    threeColorFill(CRGB::White, CRGB::Red, CRGB::Green);
+    firstRun = false;
+  }
+
+  marchCW();
+  LEDS.show();
   delay(125);
 }
+
+void flicker() {            //-m9-FLICKER EFFECT
+  int random_bright = random(0,255);
+  int random_delay = random(10,100);
+  int random_bool = random(0,random_bright);
+  if (random_bool < 10) {
+    for(int i = 0 ; i < ledCount; i++ ) {
+      leds[i] += random_bright - 180;
+    }
+    LEDS.show();
+    delay(random_delay);
+  }
+}
+
 
 void twinkle()
 {
@@ -107,7 +124,7 @@ void twinkle()
   int random_delay = random(10,100);
   int random_bool = random(0,random_bright);
   //print("seed drop ");println();
-  for(int i = 0;i<NUM_LEDS;i++)
+  for(int i = 0;i<ledCount;i++)
   {
     if (random(0,30) == 0) 
     {
@@ -126,10 +143,11 @@ void color_bounce() {                        //-m5-BOUNCE COLOR (SINGLE LED)
   static byte idex = 0;
   static byte hue = 0;
   static byte val = 255;
+
   static boolean bounceForward = true;
   if (bounceForward) {
     idex = idex + 1;
-    if (idex == NUM_LEDS) {
+    if (idex == ledCount) {
       bounceForward = false;
       idex = idex - 1;
     }
@@ -141,21 +159,16 @@ void color_bounce() {                        //-m5-BOUNCE COLOR (SINGLE LED)
       bounceForward = true;
     }
   }  
-  for(int i = 0; i < NUM_LEDS; i++ ) 
-  {
-    if (i == idex) 
-    {
-      leds[adjacent_cw(i)] = CHSV(hue, 255, val/2);
-      leds[i] = CHSV(hue, 255, val);
-      leds[adjacent_ccw(i)] = CHSV(hue, 255, val/2);
-    }
-    else 
-    {
-      leds[i] = CHSV(0, 0, 0);
-    }
-    hue = getFilteredHue(hue);
-    //val+=2;
-  }
+
+  // reset everything to black 
+  memset(leds, 0, sizeof(struct CRGB) * ledCount); 
+
+  // set the leds that we want set 
+  leds[adjacent_cw(idex)] = CHSV(hue, 255, val/2);
+  leds[idex] = CHSV(hue, 255, val);
+  leds[adjacent_ccw(idex)] = CHSV(hue, 255, val/2);
+  hue = getFilteredHue(hue);
+
   LEDS.show();
   delay(75);
 }
@@ -175,10 +188,11 @@ byte getFilteredHue(byte hue)
 
 void loop()
 {
-  for (int i = 0;i<250;i++)
-    rwb_march();
-  for (int i = 0;i<1000;i++)
-    flicker();
+  rwb_march(true);
+  for (int i = 0;i<150;i++)
+    rwb_march(false);
+//  for (int i = 0;i<1000;i++)
+//    flicker();
   for (int i = 0;i<250;i++)
     color_bounce();
   for (int i = 0;i<250;i++)
